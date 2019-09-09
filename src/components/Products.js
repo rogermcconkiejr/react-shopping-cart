30e37a38dcda24b90e77bcbac2842851a29c3633
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-09 14:05:41 -0600

    removed props from products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,15 +5,15 @@ import Product from './Product';
 // Contexts
 import {ProductContext} from "../contexts/ProductContext";
 
-const Products = props => {
+const Products = () => {
 	const { products, addItem } = useContext(ProductContext);
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
